48bcf5520cce37c8b0409808e718e75ce51a2c20
qichunren <39784+qichunren@users.noreply.github.com>
2019-05-22 18:57:35 +0800

    build sites

---
+++ build.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+d = File.expand_path("..", File.dirname(__FILE__))
+Dir.glob(d + "/*").each do |file|
+  
+  if !file.end_with?("/src")
+    puts "Remove #{file}"
+    system("rm -rf #{file}")
+  end
+end
